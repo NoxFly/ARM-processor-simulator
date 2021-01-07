0045fe8e05d50eba59359a0abdfae49392b86ac8
NoxFly <dorian.thivolle@gmail.com>
2021-01-07 11:31:37 +0100

    factorized code

--- arm_simulator-1.4/arm_data_processing.c
+++ arm_simulator-1.4/arm_data_processing.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_data_processing.h"
 #include "arm_exception.h"
@@ -27,221 +27,163 @@ Contact: Guillaume.Huard@imag.fr
 #include "util.h"
 #include "debug.h"
 
-int carryFlagAdd(uint32_t left, uint32_t right) {
+#include <string.h>
+
+int getCarryFlag(uint32_t left, uint32_t right, char operand) {
 	int cFlag = 0;
+
 	for(int i = 0 ; i < 31 ; i++) {
 		int leftBit = get_bit(left, i);
 		int rightBit = get_bit(right, i);
-		int res = leftBit + rightBit + cFlag;
-		if(res > 1) {
+
+		int res = leftBit + (operand=='+' ? rightBit : -rightBit) + cFlag;
+
+		if(operand == '+' && res > 1) {
 			cFlag = 1;
 			res -= 2;
-		}
-		else 
+		} else if(operand == '-' && (res == 2 || res == -1)) {
+			cFlag = 1;
+			res = res == 2 ? 0 : 1;
+		} else {
 			cFlag = 0;
+		}
 	}
 	
 	return cFlag;
 }
 
-int carryFlagSub(uint32_t left, uint32_t right) {
-	int cFlag = 0;
-	for(int i = 0 ; i < 31 ; i++) {
-		int leftBit = get_bit(left, i);
-		int rightBit = get_bit(right, i);
-		int res = leftBit - rightBit + cFlag;
-		if(res == 2 || res == -1) {
-			cFlag = 1;
-			res = res == 2 ? 0 : 1;
-		}
-		else
-			cFlag = 0;
-	}
-	
-	return cFlag;
+int getCarryFlagAdd(uint32_t left, uint32_t right) {
+	return getCarryFlag(left, right, '+');
+}
+
+int getCarryFlagSub(uint32_t left, uint32_t right) {
+	return getCarryFlag(left, right, '-');
+}
+
+
+int getOverflowFlag(int res, int a, int b, char operand) {
+	int leftBit = get_bit(a, 31);
+	int rightBit = get_bit(b, 31);
+
+	return
+		operand == '+' ?
+			rightBit == leftBit && rightBit != get_bit(res, 31) :
+		operand == '-' ?
+			rightBit != leftBit && rightBit != get_bit(res, 31) :
+		0;
+}
+
+int getOverflowFlagAdd(int res, int a, int b) {
+	return getOverflowFlag(res, a, b, '+');
+}
+
+int getOverflowFlagSub(int res, int a, int b) {
+	return getOverflowFlag(res, a, b, '-');
+}
+
+void updateForTest(int* updateCPSR, int* write_register) {
+	*updateCPSR = 1;
+	*write_register = 0;
 }
 
 int executeInst(int opcode, arm_core p, int rd, uint32_t valueRn, uint32_t shifter_operand, int updateCPSR) {
 	int valueCpsr = arm_read_cpsr(p);
-	int zFlag = get_bit(valueCpsr, 30);
-	int nFlag = get_bit(valueCpsr, 31);
-	int cFlag = get_bit(valueCpsr, 29);
-	int vFlag = get_bit(valueCpsr, 28);
+	int zFlag = get_bit(valueCpsr, Z);
+	int nFlag = get_bit(valueCpsr, N);
+	int cFlag = get_bit(valueCpsr, C);
+	int vFlag = get_bit(valueCpsr, V);
 	uint32_t res;
 	int write_register = 1;
-	int leftBit;
-	int rightBit;
 	int oldcFlag;
 	
 	switch(opcode) {
-		case 0b0000://AND
+		case 0b0000: // AND
 			res = valueRn && shifter_operand;
 			break;
-		case 0b0001://EOR
+		case 0b0001: // EOR
 			res = (valueRn || shifter_operand) && !(valueRn && shifter_operand);
 			break;
-		case 0b0010://SUB
+		case 0b0010: // SUB
 			res = valueRn - shifter_operand;
-			cFlag = carryFlagSub(valueRn, shifter_operand);
-			leftBit = get_bit(valueRn, 31);
-			rightBit = get_bit(shifter_operand, 31);
-			if(rightBit != leftBit && rightBit != get_bit(res, 31)) {
-				vFlag = 1;
-			}
-			else {
-				vFlag = 0;
-			}
+			cFlag = getCarryFlagSub(valueRn, shifter_operand);
+			vFlag = getOverflowFlagSub(res, valueRn, shifter_operand);
 			break;
-		case 0b0011://RSB
+		case 0b0011: // RSB
 			res = shifter_operand - valueRn;
-			cFlag = carryFlagSub(shifter_operand, valueRn);
-			leftBit = get_bit(shifter_operand, 31);
-			rightBit = get_bit(valueRn, 31);
-			if(rightBit != leftBit && rightBit != get_bit(res, 31)) {
-				vFlag = 1;
-			}
-			else {
-				vFlag = 0;
-			}
+			cFlag = getCarryFlagSub(shifter_operand, valueRn);
+			vFlag = getOverflowFlagSub(res, shifter_operand, valueRn);
 			break;
-		case 0b0100://ADD
+		case 0b0100: // ADD
 			res = shifter_operand + valueRn;
-			cFlag = carryFlagAdd(shifter_operand, valueRn);
-			leftBit = get_bit(valueRn, 31);
-			rightBit = get_bit(shifter_operand, 31);
-			if(rightBit == leftBit && rightBit != get_bit(res, 31)) {
-				vFlag = 1;
-			}
-			else {
-				vFlag = 0;
-			}
+			cFlag = getCarryFlagAdd(shifter_operand, valueRn);
+			vFlag = getOverflowFlagAdd(res, shifter_operand, valueRn);
 			break;
-		case 0b0101://ADC
+		case 0b0101: // ADC
 			res = shifter_operand + valueRn + cFlag;
 			oldcFlag = cFlag;
-			cFlag = carryFlagAdd(shifter_operand, valueRn);
-			if(cFlag == 0 && oldcFlag == 1) {
-				cFlag = carryFlagAdd(shifter_operand + valueRn, 1);
+			cFlag = getCarryFlagAdd(shifter_operand, valueRn);
+			if(!cFlag && oldcFlag) {
+				cFlag = getCarryFlagAdd(shifter_operand + valueRn, 1);
 			}
-			leftBit = get_bit(valueRn, 31);
-			rightBit = get_bit(shifter_operand, 31);
-			if(rightBit == leftBit && rightBit != get_bit(shifter_operand + valueRn, 31)) {
-				vFlag = 1;
-			}
-			else {
-				vFlag = 0;
-			}
-			if(vFlag == 0 && oldcFlag == 1) {
-				leftBit = get_bit(valueRn + shifter_operand, 31);
-				rightBit = 0;
-				if(rightBit == leftBit && rightBit != get_bit(res, 31)) {
-					vFlag = 1;
-				}
-				else {
-					vFlag = 0;
-				}
+			vFlag = getOverflowFlagAdd(shifter_operand + valueRn, shifter_operand, valueRn);
+			if(!vFlag && oldcFlag) {
+				vFlag = getOverflowFlagAdd(res, valueRn + shifter_operand, 1);
 			}
 			break;
-		case 0b0110://SBC
+		case 0b0110: // SBC
 			res = valueRn - shifter_operand - (~cFlag);
 			oldcFlag = cFlag;
-			cFlag = carryFlagSub(valueRn, shifter_operand);
-			if(cFlag == 0 && oldcFlag == 0) {
-				cFlag = carryFlagSub(valueRn - shifter_operand, 1);
+			cFlag = getCarryFlagSub(valueRn, shifter_operand);
+			if(!cFlag && !oldcFlag) {
+				cFlag = getCarryFlagSub(valueRn - shifter_operand, 1);
 			}
-			leftBit = get_bit(valueRn, 31);
-			rightBit = get_bit(shifter_operand, 31);
-			if(rightBit != leftBit && rightBit != get_bit(valueRn - shifter_operand, 31)) {
-				vFlag = 1;
-			}
-			else {
-				vFlag = 0;
-			}
-			if(vFlag == 0 && oldcFlag == 0) {
-				leftBit = get_bit(valueRn - shifter_operand, 31);
-				rightBit = 0;
-				if(rightBit != leftBit && rightBit != get_bit(res, 31)) {
-					vFlag = 1;
-				}
-				else {
-					vFlag = 0;
-				}
+			vFlag = getOverflowFlagSub(valueRn - shifter_operand, valueRn, shifter_operand);
+			if(!vFlag && !oldcFlag) {
+				vFlag = getOverflowFlagSub(res, valueRn - shifter_operand, 1);
 			}
 			break;
-		case 0b0111://RSC
+		case 0b0111: // RSC
 			res = shifter_operand - valueRn - (~cFlag);
 			oldcFlag = cFlag;
-			cFlag = carryFlagSub(shifter_operand, valueRn);
-			if(cFlag == 0 && oldcFlag == 0) {
-				cFlag = carryFlagSub(shifter_operand - valueRn, 1);
+			cFlag = getCarryFlagSub(shifter_operand, valueRn);
+			if(!cFlag && !oldcFlag) {
+				cFlag = getCarryFlagSub(shifter_operand - valueRn, 1);
 			}
-			leftBit = get_bit(shifter_operand, 31);
-			rightBit = get_bit(valueRn, 31);
-			if(rightBit != leftBit && rightBit != get_bit(shifter_operand - valueRn, 31)) {
-				vFlag = 1;
-			}
-			else {
-				vFlag = 0;
-			}
-			if(vFlag == 0 && oldcFlag == 0) {
-				leftBit = get_bit(shifter_operand - valueRn, 31);
-				rightBit = 0;
-				if(rightBit != leftBit && rightBit != get_bit(res, 31)) {
-					vFlag = 1;
-				}
-				else {
-					vFlag = 0;
-				}
+			vFlag = getOverflowFlagSub(shifter_operand - valueRn, shifter_operand, valueRn);
+			if(!vFlag && !oldcFlag) {
+				vFlag = getOverflowFlagSub(res, shifter_operand - valueRn, 1);
 			}
 			break;
-		case 0b1000://TST
+		case 0b1000: // TST
 			res = valueRn && shifter_operand;
-			updateCPSR = 1;
-			write_register = 0;
+			updateForTest(updateCPSR, write_register);
 			break;
-		case 0b1001://TEQ
+		case 0b1001: // TEQ
 			res = (valueRn || shifter_operand) && !(valueRn && shifter_operand);
-			updateCPSR = 1;
-			write_register = 0;
+			updateForTest(updateCPSR, write_register);
 			break;
-		case 0b1010://CMP
+		case 0b1010: // CMP
 			res = valueRn - shifter_operand;
-			updateCPSR = 1;
-			write_register = 0;
-			cFlag = carryFlagSub(valueRn, shifter_operand);
-			leftBit = get_bit(valueRn, 31);
-			rightBit = get_bit(shifter_operand, 31);
-			if(rightBit != leftBit && rightBit != get_bit(res, 31)) {
-				vFlag = 1;
-			}
-			else {
-				vFlag = 0;
-			}
+			updateForTest(updateCPSR, write_register);
+			cFlag = getCarryFlagSub(valueRn, shifter_operand);
+			vFlag = getOverflowFlagSub(res, valueRn, shifter_operand);
 			break;
-		case 0b1011://CMN
+		case 0b1011: // CMN
 			res = valueRn + shifter_operand;
-			updateCPSR = 1;
-			write_register = 0;
-			cFlag = carryFlagAdd(shifter_operand, valueRn);
-			leftBit = get_bit(valueRn, 31);
-			rightBit = get_bit(shifter_operand, 31);
-			if(rightBit == leftBit && rightBit != get_bit(res, 31)) {
-				vFlag = 1;
-			}
-			else {
-				vFlag = 0;
-			}
+			updateForTest(updateCPSR, write_register);
+			cFlag = getCarryFlagAdd(shifter_operand, valueRn);
+			vFlag = getOverflowFlagAdd(res, valueRn, shifter_operand);
 			break;
-		case 0b1100://ORR
+		case 0b1100: // ORR
 			res = valueRn || shifter_operand;
 			break;
-		case 0b1101://MOV
+		case 0b1101: // MOV
 			res = shifter_operand;
 			break;
-		case 0b1110://BIC
+		case 0b1110: // BIC
 			res = valueRn && (~shifter_operand);
 			break;
-		case 0b1111://MVN
+		case 0b1111: // MVN
 			res = ~shifter_operand;
 			break;
 		default:
@@ -254,28 +196,48 @@ int executeInst(int opcode, arm_core p, int rd, uint32_t valueRn, uint32_t shift
 	if(write_register) 
 		arm_write_register(p, rd, res);
 	
+	int t[4][2] = { { zFlag, Z }, { nFlag, N }, { cFlag, C }, { vFlag, V } };
+
 	if(updateCPSR) {
-		if(zFlag)
-			valueCpsr = set_bit(valueCpsr, 30);
-		else
-			valueCpsr = clr_bit(valueCpsr, 30);
-		if(nFlag)
-			valueCpsr = set_bit(valueCpsr, 31);
-		else
-			valueCpsr = clr_bit(valueCpsr, 31);
-		if(cFlag)
-			valueCpsr = set_bit(valueCpsr, 29);
-		else
-			valueCpsr = clr_bit(valueCpsr, 29);
-		if(vFlag)
-			valueCpsr = set_bit(valueCpsr, 28);
-		else
-			valueCpsr = clr_bit(valueCpsr, 28);
-		
+		for(int i=0; i < 4; i++) {
+			valueCpsr = t[i][0]? set_bit(valueCpsr, t[i][1]) : clr_bit(valueCpsr, t[i][1]);
+		}
+
 		arm_write_cpsr(p, valueCpsr);
 	}
 	
 	return 0;
+}
+
+int getShifterOperand(int so, int si, char* operand) {
+	char* operands = { "lsl", "lsr", "asr", "ror" };
+	
+	int k = -1;
+
+	for(int i=0; i < 4; i++) {
+		if(strcmp(operands[i], operand) == 0) {
+			k = i;
+		}
+	}
+
+	if(si > 0) {
+		switch(k) {
+			case 0: so = so << si; break;
+			case 1: so = so >> si; break;
+			case 2: so = asr(so, si); break;
+			case 3: so = ror(so, si);
+		}
+	}
+
+	return so;
+}
+
+int getShifterOperandImmediate(uint32_t ins, int so, char* operand) {
+	return getShifterOperand(so, get_bits(ins, 11, 7), operand);
+}
+
+int getShifterOperandRegister(arm_core p, uint32_t ins, int so, char* operand) {
+	return getShifterOperand(so, arm_read_register(p, get_bits(ins, 11, 8)), operand);
 }
 
 /* Decoding functions for different classes of instructions */
@@ -292,56 +254,14 @@ int arm_data_processing_shift(arm_core p, uint32_t ins) {
 	if(rm == 15) {
 		shifter_operand += 8;
 	}
-	if(get_bits(ins, 6, 4) == 0b000) { //LSL immediate
-		int shift_imm = get_bits(ins, 11, 7);
-		if(shift_imm > 0) {
-			shifter_operand = shifter_operand << shift_imm;
-		}
-	}
-	else if(get_bits(ins, 7, 4) == 0b0001) { //LSL register
-		int rs = get_bits(ins, 11, 8);
-		int shift_imm = arm_read_register(p, rs);
-		if(shift_imm > 0) {
-			shifter_operand = shifter_operand << shift_imm;
-		}
-	}
-	else if(get_bits(ins, 6, 4) == 0b010) { //LSR immediate
-		int shift_imm = get_bits(ins, 11, 7);
-		if(shift_imm > 0) {
-			shifter_operand = shifter_operand >> shift_imm;
-		}
-	}
-	else if(get_bits(ins, 7, 4) == 0b0011) { //LSR register
-		int rs = get_bits(ins, 11, 8);
-		int shift_imm = arm_read_register(p, rs);
-		if(shift_imm > 0) {
-			shifter_operand = shifter_operand >> shift_imm;
-		}
-	}
-	else if(get_bits(ins, 6, 4) == 0b100) { //ASR immediate
-		int shift_imm = get_bits(ins, 11, 7);
-		if(shift_imm > 0) {
-			shifter_operand = asr(shifter_operand, shift_imm);
-		}
-	}
-	else if(get_bits(ins, 7, 4) == 0b0101) { //ASR register
-		int rs = get_bits(ins, 11, 8);
-		int shift_imm = arm_read_register(p, rs);
-		if(shift_imm > 0) {
-			shifter_operand = asr(shifter_operand, shift_imm);
-		}
-	}
-	else if(get_bits(ins, 6, 4) == 0b110) { //ROR immediate
-		int shift_imm = get_bits(ins, 11, 7);
-		if(shift_imm > 0) {
-			shifter_operand = ror(shifter_operand, shift_imm);
-		}
-	}
-	else if(get_bits(ins, 7, 4) == 0b0111) { //ROR register
-		int rs = get_bits(ins, 11, 8);
-		int shift_imm = arm_read_register(p, rs);
-		if(shift_imm > 0) {
-			shifter_operand = ror(shifter_operand, shift_imm);
+
+	for(int i=0; i < 7; i++) {
+		if(get_bits(ins, 6, 4) == i) {
+			char* op = (i < 2)? "lsl" : (i < 4)? "lsr" : (i < 6)? "asr" : "ror";
+			shifter_operand = (i%2 == 0)?
+				getShifterOperandImmediate(ins, shifter_operand, op) :
+				getShifterOperandRegister(p, ins, shifter_operand, op);
+			break;
 		}
 	}
 	
